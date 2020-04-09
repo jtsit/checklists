432a1296b93e3edef987f3af6fc958443dea84ee
Ned Studt <nstudt@jtsit.net>
2020-04-09 03:35:17 -0400

    Create excel-json.ps1

---
+++ excel-json.ps1
@@ -0,0 +1,33 @@
+<#
+    .AUTHOR
+        Ned Studt
+    .SYNOPSIS
+        a factory to convert excel/json on well defined locations
+    .DESCRIPTION
+        This script is useful to transform data between .xls/csv and json formats.
+        It is a useful module to include in an agent to keep a gihub repository updates from an .xls (think IaC, ARM templates, checklists etc)
+    .USAGE
+        
+    .LICENSE
+        
+#>
+
+function fromJSON{
+
+}
+
+function toJSON{
+
+}
+
+function fromXLSX{
+
+}
+
+function toXLSX{
+
+}
+
+function Main{
+
+}
